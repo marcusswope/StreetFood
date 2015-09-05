a44e82a6d51c90e9d6342428445d0c76f538664c
Marcus Swope <marcusswope@gmail.com>
2015-09-04 20:18:11 -0500

    date formatting

--- StreetFood/Appointment.cs
+++ StreetFood/Appointment.cs
@@ -14,5 +14,20 @@ namespace StreetFood
         public string Address { get; set; }
         public float Latitude { get; set; }
         public float Longitude { get; set; }
+
+        public string TimeDescription
+        {
+            get
+            {
+                if (StartDate.Date == EndDate.Date)
+                {
+                    return string.Format("{0} {1} - {2}", StartDate.Date.ToString("M/d/yy"),
+                        StartDate.ToString("h:mmtt").ToLower(), EndDate.ToString("h:mmtt").ToLower());
+                }
+
+                return string.Format("{0} {1} - {2} {3}", StartDate.Date.ToString("M/d/yy"),
+                        StartDate.ToString("h:mmtt").ToLower(), EndDate.Date.ToString("M/d/yy"), EndDate.ToString("h:mmtt").ToLower());
+            }
+        }
     }
 }
